c507aa1207ebe7bfc8cf48f95c266d4ae4279744
Jared Fleming <jaredmf10@gmail.com>
2020-01-24 16:01:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -249,6 +249,6 @@ end
 #
 # # # Super Bonus Question
 #
-# def long_name_steals_a_ton?
-#   player_with_most_of(:steals) == player_with_most_of(:player_name)
-# end
+def long_name_steals_a_ton?
+  player_with_most_of(:steals) == player_with_most_of(:player_name)
+end
