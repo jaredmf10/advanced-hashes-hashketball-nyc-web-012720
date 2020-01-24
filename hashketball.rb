02805d2018b92eb2f02fedfbf8827e6e3c3eadfe
Jared Fleming <jaredmf10@gmail.com>
2020-01-24 16:00:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -228,18 +228,18 @@ def most_points_scored
   player_with_most_of(:points)
 end
 #
-# def winning_team
+def winning_team
 #   # Set up a hash to keep track of the points scored by each team. This way, we
 #   # can iterate through each player, get their points scored, and increase the
 #   # count in the hash.
 #
-#   scores = { 'Brooklyn Nets' => 0, 'Charlotte Hornets' => 0 }
-#
-#   game_hash.each do |_team, game_data|
-#     game_data[:players].each do |player|
-#       scores[game_data[:team_name]] += iterate_through_players_for(player[:player_name], :points)
-#     end
-#   end
+  scores = { 'Brooklyn Nets' => 0, 'Charlotte Hornets' => 0 }
+
+  game_hash.each do |_team, game_data|
+    game_data[:players].each do |player|
+      scores[game_data[:team_name]] += iterate_through_players_for(player[:player_name], :points)
+    end
+  end
 #
 #   scores.max_by { |_k, v| v }.first
 # end
